8d38a71c90aab7a60ae5dd9e589c0e0ab8d2939a
Monica Sun <msun07@gmail.com>
2015-05-16 13:34:47 -0700

    Added initial filter on categories.

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -2,13 +2,13 @@
 //  BusinessesViewController.swift
 //  Yelp
 //
-//  Created by  Monica Sun on 4/23/15.
+//  Created by  Monica Sun on 5/15/15.
 //  Copyright (c) 2015  Monica Sun. All rights reserved.
 //
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate {
 
     var businesses: [Business]!
     
@@ -71,20 +71,25 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         
         return cell
     }
-
     
-    
-    
-    
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let navigationController = segue.destinationViewController as! UINavigationController
+        let filtersViewController = navigationController.topViewController as! FiltersViewController
+        filtersViewController.delegate = self
     }
-    */
+    
+    func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
+        var searchTerm = "restaurant"
+        
+        var categories = filters["categories"] as? [String]
+        
+        Business.searchWithTerm(searchTerm, sort: nil, categories: categories, deals: nil) {
+            (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+    }
+
 
 }
